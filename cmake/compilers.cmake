efde9cb8e7fbc274efdf018f9cc1379bd5288b67
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-23 19:58:38 -0500

    ci template
    init
    
    ci template
    
    ci template

---
+++ cmake/compilers.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+
+if(CMAKE_BUILD_TYPE STREQUAL Debug)
+  add_compile_options(-g -O0)
+else()
+  add_compile_options(-O3)
+endif()
+
+if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+  add_compile_options(-march=native -fexceptions -Werror=array-bounds -Wno-unused-variable)
+  list(APPEND CFLAGS -Wno-unused-result -Wno-implicit-int)
+endif()
